a1a9d377bea82568c3186dc8dd582f05962a8a1b
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-11-10 13:08:23 +0200

    My Reservation Card Styling

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -4,10 +4,12 @@ import PageTitle from '../styled/PageTitle'
 import styled from 'styled-components'
 import Hr from '../styled/Hr'
 import { Row, Col } from 'react-simple-flex-grid'
+import moment from 'moment'
 
 const ReservationCardContainer = styled.div`
   border-radius: 0.8rem;
-  height: 15rem;
+  /* height: 15rem; */
+  padding-bottom: 1rem;
   margin: 0 auto;
   background: linear-gradient(316.33deg, #485a7d 0%, #071839 100%);
   margin-top: 1rem;
@@ -38,15 +40,85 @@ export default function MyReservations() {
           {reservations.map(r => (
             <Col key={r.id} xs={12} sm={4} md={4} lg={3} xl={3}>
               <ReservationCardContainer>
-                <div style={{ color: 'white' }}>{r.date}</div>
-                <div style={{ color: 'white' }}>{r.time}</div>
-                <div style={{ color: 'white' }}>{r.restaurantData.title}</div>
-                <div style={{ color: 'white' }}>
+                <div
+                  style={{
+                    color: 'white',
+                    textAlign: 'left',
+                    fontWeight: 500,
+                    fontSize: '1.4rem',
+                    paddingTop: '0.6rem',
+                    paddingLeft: '1rem'
+                  }}
+                >
+                  {moment(r.date).format('Do')}
+                </div>
+                <div
+                  style={{
+                    color: 'white',
+                    textAlign: 'left',
+                    fontWeight: 500,
+                    fontSize: '1.4rem',
+                    paddingLeft: '1rem'
+                  }}
+                >
+                  {moment(r.time).format('MMMM, dddd')}
+                </div>
+                <div
+                  style={{
+                    color: 'white',
+                    textAlign: 'left',
+                    fontWeight: 900,
+                    fontSize: '1.1rem',
+                    paddingTop: '1.5rem',
+                    paddingLeft: '1.5rem'
+                  }}
+                >
+                  {r.restaurantData.title}
+                </div>
+                <div
+                  style={{
+                    color: 'white',
+                    textAlign: 'left',
+                    fontWeight: 300,
+                    fontSize: '1.1rem',
+                    paddingLeft: '2rem'
+                  }}
+                >
                   {r.restaurantData.location}
                 </div>
-                <div style={{ color: 'white' }}>{r.restaurantData.phone}</div>
-                <div style={{ color: 'white' }}>{r.numberOfPeople}</div>
-                <div style={{ color: 'white' }}>{r.smoking}</div>
+                <div
+                  style={{
+                    color: 'white',
+                    textAlign: 'left',
+                    fontWeight: 300,
+                    fontSize: '1.1rem',
+                    paddingLeft: '2rem'
+                  }}
+                >
+                  {r.restaurantData.phone}
+                </div>
+                <div
+                  style={{
+                    color: 'white',
+                    paddingTop: '1rem',
+                    textAlign: 'left',
+                    fontWeight: 500,
+                    paddingLeft: '1.5rem'
+                  }}
+                >
+                  {r.numberOfPeople}{' '}
+                  {r.numberOfPeople === '1' ? 'Person' : 'People'}
+                </div>
+                <div
+                  style={{
+                    color: 'white',
+                    textAlign: 'left',
+                    fontWeight: 500,
+                    paddingLeft: '1.5rem'
+                  }}
+                >
+                  {r.smoking}
+                </div>
               </ReservationCardContainer>
             </Col>
           ))}
